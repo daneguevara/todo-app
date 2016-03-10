381c424bd928a36a23e18142735571a33733ccb2
Dane Guevara <dane.guevara@cerner.com>
2016-03-09 18:41:19 -0600

    Add todo completion and deletion and update UI project structure.

--- client.js
+++ client.js
@@ -3,9 +3,14 @@ var express = require('express');
 var app = express();
 var port = 8080;
 
+// set static file location
+app.use(express.static(__dirname + '/public'));
+
+// route all requests to a single page web application
 app.get('*', function(req, res) {
-    res.sendfile('index.html'); // load single page web application
+    res.sendFile(__dirname + '/public/index.html'); //
 });
 
+// start server
 app.listen(port);
 console.log('Listening on port ' + port);
---
+++ public/core.js
@@ -0,0 +1,66 @@
+﻿var todoApp = angular.module('todoApp', []);
+var todoServerURI = "http://localhost:9999"; // TODO: modularize todo server configuration
+
+function mainController($scope, $http) {
+  clearForm();
+  syncTodos();
+
+  $scope.createTodo = function () {
+    $http.post(todoServerURI + '/api/todos', $scope.formData)
+      .success(function () {
+        clearForm();
+        syncTodos();
+      })
+      .error(function (data) {
+        console.log('Error: ' + data);
+      });
+  };
+
+  $scope.completeTodo = function (todo) {
+    $http.put(todoServerURI + '/api/todos/' + todo._id, todo)
+      .success(function () {
+        syncTodos();
+      })
+      .error(function (data) {
+        console.log('Error: ' + data);
+      });
+  };
+
+  $scope.deleteTodo = function (todo) {
+    $http.delete(todoServerURI + '/api/todos/' + todo._id, todo)
+      .success(function () {
+        syncTodos();
+      })
+      .error(function (data) {
+        console.log('Error: ' + data);
+      });
+  };
+
+  $scope.active = function() {
+    return $scope.todos.filter(function(todo) {
+      return !todo.done;
+    }).length;
+  };
+
+  $scope.completed = function () {
+    return $scope.todos.filter(function(todo) {
+      return todo.done;
+    }).length;
+  };
+
+  function clearForm() {
+    $scope.formData = {};
+  }
+
+  function syncTodos() {
+    $http.get(todoServerURI + '/api/todos')
+    .success(function (data) {
+      $scope.todos = data || [];
+
+      console.log(data);
+    })
+    .error(function (data) {
+      console.log('Error: ' + data);
+    });
+  }
+}
